54be62169d79cd7a09b08f4abd518aa4af17d1ec
David S <david.shams@ascension-external.com>
2019-10-25 14:18:16 -0500

    ascension custom card initial commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './card.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
-const Card = (props) => {
+const ACard = (props) => {
   return (
-    <div className="card">
-      {props.children}
+    <div>
+
+    <Card style={{ width: '18rem' }}>
+      {<Card.Img variant="top" src="https://ascensiononlinecare.org/static/release_e3aaf4f43d050213e79ec46711a1271e46b41fff/images/common/IMG_Login_FormTopLogo.jpg" /> }
+      <Card.Body>
+        <Card.Title>Waiting Room</Card.Title>
+        <Card.Text>
+            {props.children}
+        </Card.Text>
+        <Button variant="primary">Book appointment</Button>
+      </Card.Body>
+    </Card>
     </div>
   );
 };
 
-export default Card;
+export default ACard;
